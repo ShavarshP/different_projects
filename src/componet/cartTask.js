c7ed9c6e89a492e07d26502ff0bc96764076cef3
ShavarshP <papoyan101097@mail.ru>
2021-07-20 14:13:36 +0400

    add: created todo

--- src/componet/cartTask.js
+++ src/componet/cartTask.js
@@ -1,18 +1,101 @@
 const CartTask = (props) => {
+  const deletef = (task) => {
+    props.delete(task);
+  };
+  const process = (task) => {
+    props.process(task);
+  };
+  if (props.conditions) {
+    return (
+      <div className="bg-gray-100 rounded-xl p-2 mb-2 bg-green-100">
+        <div className="flex">
+          <div className="flex-auto pl-2">
+            <div className="leading-tight text-gray-500 line-through">
+              {props.task}
+            </div>
+          </div>
+          <div className="flex-none pr-2">
+            <div className="text-red-500 hover:text-red-600 float-right">
+              <span className="fa fa-minus-circle"></span>
+            </div>
+          </div>
+          <div className="flex-none">
+            <div className="text-green-500 hover:text-red-600 float-right">
+              <span
+                className="fa fa-check-circle"
+                onClick={() => deletef(props.task)}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
   return (
     <div className="bg-gray-100 rounded-xl p-2 mb-2 bg-gray-100">
       <div className="flex">
         <div className="flex-auto pl-2">
-          <div className="leading-tight text-gray-500">Make Dinner</div>
+          <div className="leading-tight text-gray-500">{props.task}</div>
         </div>
         <div className="flex-none pr-2">
           <div className="text-red-500 hover:text-red-600 float-right">
-            <span className="fa fa-minus-circle"></span>
+            <span
+              className="fa fa-minus-circle"
+              onClick={() => process(props.task)}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M5 13l4 4L19 7"
+                />
+              </svg>
+            </span>
           </div>
         </div>
         <div className="flex-none">
           <div className="text-green-500 hover:text-red-600 float-right">
-            <span className="fa fa-check-circle"></span>
+            <span
+              className="fa fa-check-circle"
+              onClick={() => deletef(props.task)}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </span>
           </div>
         </div>
       </div>
--- src/page/todo.js
+++ src/page/todo.js
@@ -14,17 +14,85 @@ class Todo extends React.Component {
 
   componentDidMount() {}
 
+  add = () => {
+    console.log(this.state.newTask);
+    if (this.state.newTask) {
+      this.setState({
+        tasks: [
+          ...this.state.tasks,
+          { value: this.state.newTask, isprocess: false },
+        ],
+        newTask: "",
+      });
+    }
+  };
+
+  onChangeHandler = (event) => {
+    this.setState({ newTask: event.target.value });
+  };
+
+  delete = (dTask) => {
+    this.setState({
+      tasks: [
+        ...this.state.tasks.filter((value) => {
+          if (value.value !== dTask) {
+            return value;
+          }
+        }),
+      ],
+    });
+  };
+  process = (dTask) => {
+    this.setState({
+      tasks: [
+        ...this.state.tasks.map((t) => {
+          if (t.value === dTask) {
+            return { value: t.value, isprocess: true };
+          }
+          return t;
+        }),
+      ],
+    });
+  };
+
   render() {
-    let tasks = this.state.tasks.map((task) => {
-      return <CartTask task={task} />;
+    let tasks = this.state.tasks.map((task, index) => {
+      return (
+        <CartTask
+          task={task.value}
+          key={index}
+          conditions={task.isprocess}
+          delete={this.delete}
+          process={this.process}
+        />
+      );
     });
     return (
       <div className="flex flex-col items-center min-h-screen bg-gray-200 justify-center py-10">
         <div className="bg-white rounded-xl p-4 w-80 shadow">
           <div className="flex">
-            <input className="flex-initial bg-gray-100 rounded-xl p-2 mb-2 focus:outline-none focus:ring-2 focus:ring-blue-600 w-full mr-2 pl-4"></input>
+            <input
+              onChange={this.onChangeHandler}
+              value={this.state.newTask}
+              className="flex-initial bg-gray-100 rounded-xl p-2 mb-2 focus:outline-none focus:ring-2 focus:ring-blue-600 w-full mr-2 pl-4"
+            ></input>
             <button className="flex-initial bg-blue-100 hover:bg-blue-200 rounded-xl p-2 mb-2 focus:outline-none focus:ring-2 focus:ring-blue-600 w-20">
-              <span className="fa fa-plus text-blue-500"></span>
+              <span className="text-blue-500" onClick={this.add}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-12"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+                  />
+                </svg>
+              </span>
             </button>
           </div>
           {tasks}
