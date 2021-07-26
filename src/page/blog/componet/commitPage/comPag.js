218478ac9c32f9c8a836fdd62217b72bcee2fd2d
ShavarshP <papoyan101097@mail.ru>
2021-07-26 20:57:43 +0400

    fix: bage

--- src/page/blog/componet/commitPage/comPag.js
+++ src/page/blog/componet/commitPage/comPag.js
@@ -2,6 +2,7 @@ import React from "react";
 import ComPost from "../posts/commitPost";
 import Commits from "../posts/commits/commits";
 import { withRouter } from "react-router";
+import { Redirect } from "react-router-dom";
 
 class ComPag extends React.Component {
   constructor(props) {
@@ -9,7 +10,7 @@ class ComPag extends React.Component {
     this.state = {
       newCommit: "",
       posts: this.getData(),
-      render: true,
+      render: false,
     };
   }
   getData = () => {
@@ -20,12 +21,18 @@ class ComPag extends React.Component {
     return data.filter((item) => Number(item.id) === Number(id))[0];
   };
 
-  trashPost = (id) => {
-    // const data = JSON.parse(localStorage.getItem("store")).filter(
-    //   (item) => item.id !== id
-    // );
-    // localStorage.setItem("store", JSON.stringify(data));
-    // this.setState({render:false})
+  trashPost = (idd) => {
+    const data = JSON.parse(localStorage.getItem("store"));
+    const url = window.location.pathname.split("/");
+    const id = url[url.length - 1];
+    const username = JSON.parse(localStorage.getItem("auth")).username;
+    console.log(data[0].name);
+
+    const newdata = data.filter(
+      (item) => !(Number(item.id) === Number(id) && item.name === username)
+    );
+    localStorage.setItem("store", JSON.stringify(newdata));
+    this.setState({ render: true });
   };
 
   addCommit = () => {
@@ -59,7 +66,9 @@ class ComPag extends React.Component {
   };
 
   render() {
-    console.log(window.localStorage.id);
+    if (this.state.render) {
+      return <Redirect to={"/blog/home"} />;
+    }
     const commit = this.state.posts.coment.map((item) => (
       <Commits name={item.name} commit={item.commit} />
     ));
